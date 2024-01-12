2c931ee558308d1cd66056ec2e3e219842d93399
Илья Зуев <zueviliya777@gmail.com>
2024-01-12 07:32:43 +0300

    Все задания

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace FinalTask
+﻿using System.Globalization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string path = "C:\\Users\\zuevi\\Desktop\\Students";
+            Directory.CreateDirectory(path);
+
+            string filePath = "C:\\Users\\zuevi\\Downloads\\Students.dat";
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            var fileStream = new FileStream(filePath, FileMode.Open);
+            try
+            {
+                var students = (Student[])formatter.Deserialize(fileStream);
+                foreach (var student in students)
+                {
+                    StreamWriter streamWriter = new StreamWriter(path + "/" + student.Group + ".txt", true);
+                    streamWriter.WriteLine($"{student.Name}, {student.DateOfBirth.ToShortDateString()}");
+                    streamWriter.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
--- FinalTask/Student.cs
+++ FinalTask/Student.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace FinalTask
 {
-    internal class Student
+    [Serializable]
+    class Student
     {
+        public string Name {  get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
     }
 }
